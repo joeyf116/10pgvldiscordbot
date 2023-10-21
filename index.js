037a39fcab90570a60efce2ac52bc62e607c7f63
Joey Fitzpatrick <joeyf116@yahoo.com>
2023-10-21 18:13:17 -0400

    !ses command

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-const OpenAI = require("openai");
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const OpenAIApi = require("openai");
+const openai = new OpenAIApi({ apiKey: process.env.OPENAI_API_KEY });
 //In bot.js
 const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
 });
 
 const commands = [
@@ -22,35 +26,55 @@ rest
 	.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
 	.then((res) => console.log(res))
 	.catch(console.error);
-console.log("Successfully reloaded application (/) commands.");
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", async (message) => {
-	if (message.content.substring(0, 1) === "!") {
-		const prompt = message.content.substring(1); //remove the exclamation mark from the message
-		const answer = await ask(prompt); //prompt GPT-3
-		message.channel.send(answer); //reply to Discord with answer from GPT-3
+client.on("messageCreate", async (message) => {
+	if (message.author.bot || !message.content.startsWith("!ses")) return;
+
+	try {
+		const gptResponse = await openai.chat.completions.create({
+			messages: [
+				{
+					role: "user",
+					content: message.content,
+				},
+			],
+			temperature: 0.9,
+			max_tokens: 100,
+			model: "gpt-3.5-turbo",
+		});
+		message.reply(gptResponse.choices[0].message.content);
+	} catch (err) {
+		message.reply("Let Joey know that something went wrong.");
 	}
 });
 
 client.on("interactionCreate", async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 
-	if (interaction.commandName === "insult") {
-		const chatCompletion = await openai.chat.completions.create({
-			messages: [
-				{
-					role: "user",
-					content: "In 20 words max, write an insult about someones Jiu Jitsu and why they lack everything that makes a good Jiu Jitsu player.",
-				},
-			],
-			model: "gpt-3.5-turbo",
-		});
-		await interaction.reply(chatCompletion.choices[0].message.content);
+	try {
+		if (interaction.commandName === "insult") {
+			const chatCompletion = await openai.chat.completions.create({
+				messages: [
+					{
+						role: "user",
+						content:
+							"In 20 words max, write an insult about someones Jiu Jitsu and why they lack everything that makes a good Jiu Jitsu player.",
+					},
+				],
+				model: "gpt-3.5-turbo",
+				temperature: 0.9,
+			});
+			await interaction.reply(chatCompletion.choices[0].message.content);
+		}
+	} catch (err) {
+		console.log(err);
 	}
+
+	interaction.reply("Let Joey know that something went wrong.");
 });
 
 client.login(process.env.DISCORD_TOKEN);
@@ -61,6 +85,7 @@ app.get("/", (req, res) => {
 });
 
 const port = parseInt(process.env.PORT) || 8080;
+
 app.listen(port, () => {
 	console.log(`helloworld: listening on port ${port}`);
 });
